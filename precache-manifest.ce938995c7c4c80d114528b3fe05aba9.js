da5b05bff3a35157c0a9476b3b3ec09300771f81
Henrique Martins <enrque.ns@gmail.com>
2019-04-21 16:43:29 -0300

    Updates

--- precache-manifest.ce938995c7c4c80d114528b3fe05aba9.js
+++ precache-manifest.ce938995c7c4c80d114528b3fe05aba9.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/regas-jogo-wiki/static/js/runtime~main.b85aaeaf.js"
   },
   {
-    "revision": "bec632cb8808d8fe3bfd",
-    "url": "/regas-jogo-wiki/static/js/main.4384fcce.chunk.js"
+    "revision": "213eede57f975331842b",
+    "url": "/regas-jogo-wiki/static/js/main.7d049ebc.chunk.js"
   },
   {
     "revision": "a6f4d53dac062a0aac87",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/regas-jogo-wiki/static/css/2.31d801df.chunk.css"
   },
   {
-    "revision": "e476ff22db0ba9f4315f1b81a3965baf",
+    "revision": "3d54467df0939943487d4d032ef70d9e",
     "url": "/regas-jogo-wiki/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/regas-jogo-wiki/precache-manifest.19de8c289afc1307ff12b985f03d28fb.js"
+  "/regas-jogo-wiki/precache-manifest.ce938995c7c4c80d114528b3fe05aba9.js"
 );
 
 workbox.clientsClaim();
